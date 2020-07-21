17c16abf05fc3422a1042e0d338eb5a33fd5f445
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-22 00:54:19 +0500

    updating

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 
 const person = require("./pictures/person.gif");
 const bull = require("./pictures/bull.gif");
-const grass = require("./pictures/grass.jpeg");
+const grass = require("./pictures/grass.png");
 const helicopter = require("./pictures/helicopter.gif");
-const forest = require("./pictures/forest.jpg");
+const forest = require("./pictures/forest.png");
+const boundary = require("./pictures/boundary.png");
 
 const road = require("./pictures/road.jpg");
 
@@ -22,12 +23,54 @@ const house4 = require("./pictures/house4.png");
 const house5 = require("./pictures/house5.png");
 const house6 = require("./pictures/house6.png");
 
-const App = () => {
+function App() {
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [
+      { transform: "translateX(-210%)" },
+      { transform: "translateX(100%)" },
+    ],
+    timing: {
+      duration: 10000,
+      iterations: Infinity,
+      easing: "ease-in-out",
+      direction: "reverse",
+    },
+  });
+
   return (
-    <div>
-      <h1>Web Animation!!</h1>
+    <div
+      onClick={() =>
+        getAnimation().updatePlaybackRate(getAnimation().playbackRate * 1.2)
+      }
+    >
+      <div>
+        <img src={forest} className="forest" />
+      </div>
+      <div ref={ref}>
+        <div>
+          <img src={tree1} className="tree1" />
+          <img src={tree2} className="tree2" />
+          <img src={tree3} className="tree3" />
+          <img src={tree4} className="tree4" />
+          <div>
+            <img src={house1} className="house1" />
+            <img src={house2} className="house2" />
+            <img src={house3} className="house3" />
+            <img src={house4} className="house4" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={road} className="road" />
+      </div>
+      <div>
+        <img src={person} className="person" />
+      </div>
+      <div>
+        <img src={grass} className="grass" />
+      </div>
     </div>
   );
-};
+}
 
 export default App;
