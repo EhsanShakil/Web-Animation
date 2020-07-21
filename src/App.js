4864717b3e07bb5f15e45e208a7c926fc716be27
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-21 23:04:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React from "react";
+import "./App.css";
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+const person = require("./pictures/person.gif");
+const bull = require("./pictures/bull.gif");
+const grass = require("./pictures/grass.jpeg");
+const helicopter = require("./pictures/helicopter.gif");
+const forest = require("./pictures/forest.jpg");
+
+const road = require("./pictures/road.jpg");
+
+const tree1 = require("./pictures/tree1.png");
+const tree2 = require("./pictures/tree2.png");
+const tree3 = require("./pictures/tree3.png");
+const tree4 = require("./pictures/tree4.png");
+
+const house1 = require("./pictures/house1.png");
+const house2 = require("./pictures/house2.png");
+const house3 = require("./pictures/house3.png");
+const house4 = require("./pictures/house4.png");
+const house5 = require("./pictures/house5.png");
+const house6 = require("./pictures/house6.png");
 
 const App = () => {
   return (
